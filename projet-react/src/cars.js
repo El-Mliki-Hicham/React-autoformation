7e9150bad57557e8c3e8aa78b5c98927f7699297
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-08-02 15:14:04 +0200

    arrow function

--- projet-react/src/cars.js
+++ projet-react/src/cars.js
@@ -4,6 +4,7 @@ export const car = ["car1" ,"car2"];
 
 
 class Cars extends React.Component{
+names = ()=> 'Cars Name : ';
     state = {
         car1 : car[0],
         car2 : car[1]
@@ -11,11 +12,12 @@ class Cars extends React.Component{
 render(){
     return(
         <div>
-            <h1>Cars name  : </h1>
+            <h1>{this.names()}</h1>
             <h2>{this.state.car1}</h2>
             <h2>{this.state.car2}</h2>
         </div>
     )
 }
 }
+
 export default Cars ;
