893d30aea4db44099ab20a6ffc177f6e28c13267
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-08-02 14:34:46 +0200

    export comonent

--- projet-react/src/App.js
+++ projet-react/src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 import React from 'react';
-import {cars as x} from "./cars";
+import Cars from "./cars";
+
 
 class App extends React.Component{
   render(){
     return (
       <div>
-        <h1>{x[0]}</h1>
-        <h1>{x[1]}</h1>
+        
+        <Cars />
       </div>
     )
   }
--- projet-react/src/cars.js
+++ projet-react/src/cars.js
@@ -1,2 +1,21 @@
-export const cars = ["car1" ,"car2"];
+import React, { Component } from 'react';
 
+export const car = ["car1" ,"car2"];
+
+
+class Cars extends React.Component{
+    state = {
+        car1 : car[0],
+        car2 : car[1]
+    }
+render(){
+    return(
+        <div>
+            <h1>Cars name  : </h1>
+            <h2>{this.state.car1}</h2>
+            <h2>{this.state.car2}</h2>
+        </div>
+    )
+}
+}
+export default Cars ;
