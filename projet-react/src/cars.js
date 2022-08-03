f6268749c3409b9197eaece81d8c7b099af44430
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-08-03 14:06:53 +0200

    Event

--- projet-react/src/cars.js
+++ projet-react/src/cars.js
@@ -4,18 +4,26 @@ export const car = ["car1" ,"car2"];
 
 
 class Cars extends React.Component{
-names = ()=> 'Cars Name : ';
+    
+btn =()=>{
+    return alert(this.state.car1);
+}
+    names = ()=> 'Cars Name : ';
     state = {
         car1 : car[0],
         car2 : car[1]
-    }
+ 
+}
 render(){
     return(
         <div>
+            
             <h1>{this.names()}</h1>
             <h2>{this.state.car1}</h2>
             <h2>{this.state.car2}</h2>
+            <button onClick={this.btn}>click</button>
         </div>
+
     )
 }
 }
