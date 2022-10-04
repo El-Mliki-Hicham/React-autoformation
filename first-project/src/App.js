5292af278b3fc3d2cedc25d35a098d5ad4191547
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-10-04 22:24:14 +0200

    import template login

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -3,17 +3,19 @@ import Home from './components/home';
 import Person from './components/person';
 import Navbar from './components/navbar';
 import { BrowserRouter ,Routes,Route, Link} from "react-router-dom";
+import Login from './components/login';
 
 class App extends Component {
 render(){
     return(
-      <BrowserRouter>
-      <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />}> </Route>
-      <Route path="/Persons" element={<Person />}> </Route>
-    </Routes>
-    </BrowserRouter >
+      <Login/>
+    //   <BrowserRouter>
+    //   <Navbar />
+    // <Routes>
+    //   <Route path="/" element={<Home />}> </Route>
+    //   <Route path="/Persons" element={<Person />}> </Route>
+    // </Routes>
+    // </BrowserRouter >
     )
 }
 }
