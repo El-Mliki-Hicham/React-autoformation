60f90835a8e64d51551b2cb48281ce75f73900a0
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-10-27 03:42:37 -0700

    use effect

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import PageUseState from './UseState/useState';
+import PageUseEffect from './UseEffect/useEffect';
 
 function App() {
   return (
     
       <div>
       <PageUseState />
+      <PageUseEffect />
       </div>
   );
 }
