2415273daa80dae6735c97a2050e22f60d94f6ca
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-10-27 03:10:51 -0700

    use state exemple

--- hooks/src/UseState/useState.js
+++ hooks/src/UseState/useState.js
@@ -3,16 +3,16 @@ import { useState } from "react";
 
 
 const PageUseState=()=>{
-    const [name , setName]  = useState("Hicham");
+    const [name , setName]  = useState("hicham");
     
-
    const setData=()=>{
         setName('bilal')
     }   
 
     return(
     <div>
-       <h1> hello {name}</h1>
+        <h1 style={{color:'red'}}>Use state exemple </h1>
+       <h3> hello {name}</h3>
        <button onClick={setData}> set name</button>
     </div>
 )
