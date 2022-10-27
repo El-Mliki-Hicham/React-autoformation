298faed6d37d9bb9213c98702894c7f4acb7c645
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-10-27 05:28:00 -0700

    useref

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import PageUseState from './UseState/useState';
 import PageUseEffect from './UseEffect/useEffect';
+import PageUseRef from './UseRef/useRef';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div>
       <PageUseState />
       <PageUseEffect />
+      <PageUseRef />
       </div>
   );
 }
